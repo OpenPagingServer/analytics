843c4491ccba605e7dc13b60f65e51cae8a65ce8
CaptainChris <49162818+CaptainChris2019@users.noreply.github.com>
2026-05-20 20:59:25 -0700

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+$analyticsDsn = 'mysql:host=localhost;dbname=database;charset=utf8mb4';
+$analyticsDbUser = 'database';
+$analyticsDbPass = 'password';
+$analyticsDefaultTimezone = 'UTC';
+$analyticsTimezoneLookupUrl = 'http://ip-api.com/json/%s?fields=status,timezone';
